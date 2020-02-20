b93e442d3f0ee2a714647c10dff35a83480896d0
dj43780 <35635914+dj34780@users.noreply.github.com>
2020-02-20 18:29:45 -0500

    teleop done.

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,162 +1,292 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+
 
 #include "Robot.h"
 
 #include <iostream>
 
 #include <frc/smartdashboard/SmartDashboard.h>
+
 #define _USE_MATH_DEFINES
+
 #include <cmath>
 
 using namespace frc;
+
 Robot::Robot(){
-//  WPI_TalonSRX * motor2 = new WPI_TalonSRX(2);
-//  WPI_TalonSRX * motor1 = new WPI_TalonSRX(1);
-
-
-
+    //  WPI_TalonSRX * motor2 = new WPI_TalonSRX(2);
+    //  WPI_TalonSRX * motor1 = new WPI_TalonSRX(1);
 
 }
 void Robot::RobotInit() {
-  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
-  //motor.Set(ControlMode::PercentOutput,.1);
-  
+    m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+    m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+    frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+    //motor.Set(ControlMode::PercentOutput,.1);
+
+
 }
 
-/**
- * This function is called every robot packet, no matter the mode. Use
- * this for items like diagnostics that you want ran during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
-void Robot::RobotPeriodic() {}
+void Robot::RobotPeriodic() {
 
-/**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
- */
+}
+
 void Robot::AutonomousInit() {
-  m_autoSelected = m_chooser.GetSelected();
-  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
-  //     kAutoNameDefault);
-  std::cout << "Auto selected: " << m_autoSelected << std::endl;
+    m_autoSelected = m_chooser.GetSelected();
+    // m_autoSelected = SmartDashboard::GetString("Auto Selector",
+    //     kAutoNameDefault);
+    // std::cout << "Auto selected: " << m_autoSelected << std::endl;
 
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
-  }
+    if (m_autoSelected == kAutoNameCustom) {
+        // Custom Auto goes here
+    } else {
+        // Default Auto goes here
+
+    }
 }
 
 void Robot::AutonomousPeriodic() {
+    updateEncoderCount();
+    leftDistanceTraveledFt = (( M_PI * wheelDiameterFt)/ encoderCPR) * -leftEncoderCount;
+    rightDistanceTraveledFt = ((M_PI * wheelDiameterFt)/ encoderCPR) * rightEncoderCount;
 
-updateEncoderCount();
-leftDistanceTraveledFt = (( M_PI * wheelDiameterFt)/ encoderCPR) * leftEncoderCount;
-rightDistanceTraveledFt = ((M_PI * wheelDiameterFt)/ encoderCPR) * rightEncoderCount;
+    if (rightDistanceTraveledFt < goalDistance && leftDistanceTraveledFt < goalDistance){
+        drivetrain.ArcadeDrive(-0.5,0);
+    }else {
+        drivetrain.ArcadeDrive(0,0);
+    }
+    frc::SmartDashboard::PutNumber("Distance traveled left",leftDistanceTraveledFt);
+    frc::SmartDashboard::PutNumber("Distance traveled right",rightDistanceTraveledFt);
+    frc::SmartDashboard::PutNumber("Right encoder count", rightEncoderCount);
+    frc::SmartDashboard::PutNumber("Left encoder count", leftEncoderCount);
 
-if (rightDistanceTraveledFt < goalDistance && leftDistanceTraveledFt < goalDistance){
-  drivetrain.ArcadeDrive(-0.5,0);
-}else {
-  drivetrain.ArcadeDrive(0,0);
-}
-
-  // if (m_autoSelected == kAutoNameCustom) {
-  //   // Custom Auto goes here
-  // } else {
-  //   // Default Auto goes here
-  // }
+    // if (m_autoSelected == kAutoNameCustom) {
+    //   // Custom Auto goes here
+    // } else {
+    //   // Default Auto goes here
+    // }
 }
 
 void Robot::TeleopInit() {
-  time.Start();
+    time.Start();
 }
 
 void Robot::TeleopPeriodic() {
-  double currentTime = time.Get();
-  double leftX = controller.GetX(GenericHID::JoystickHand::kLeftHand);
-  double leftY = controller.GetY(GenericHID::JoystickHand::kLeftHand);
-  double filtX = (1 - a) * prevLeftX + a*leftX;
-  double filtY = (1 - a) * prevLeftY + a*leftY;
-  prevLeftX = filtX;
-  prevLeftY = filtY;
 
+    double currentTime = time.Get();
+    double rightX = controller.GetX(GenericHID::JoystickHand::kRightHand);
+    double leftY = controller.GetY(GenericHID::JoystickHand::kLeftHand);
+    double filtX = (1 - a) * prevRightX + a * rightX;
+    double filtY = (1 - a) * prevLeftY + a * leftY;
+    prevRightX = filtX;
+    prevLeftY = filtY;
 
-  // std::cout << "joy:" << controller.GetX( GenericHID::JoystickHand::kLeftHand) << std::endl;
-  // std::cout << "filtX" << filtX << std::endl;
-  // std::cout << "filtY" <<filtY << std::endl;
-  // std::cout << "a = " << a << std::endl;
-  //std :: cout
-  // std::cout << controller.GetX(0) << std::endl;
-  
-//  if (controller.GetAButton() == true){
-//    shooterMotor.Set(ControlMode::PercentOutput, 0.25);
-//    std::cout<<"25%"<<std::endl;
-
-//  }else if (controller.GetBButton() == true) {
-//    shooterMotor.Set(ControlMode::PercentOutput, 0.5);
-//    std::cout<<"50%"<<std::endl;
-
-//  }else if (controller.GetYButton() == true){
-//    shooterMotor.Set(ControlMode::PercentOutput, 0.75);
-//    std::cout<<"75%"<<std::endl;
-
-//  }else if (controller.GetXButton() == true){
-//    shooterMotor.Set(ControlMode::PercentOutput, 1.0);
-//    std::cout<<"100%"<<std::endl;
-
-//  }else{
-//   shooterMotor.Set(ControlMode::PercentOutput,0);
-//   std::cout<<"0%"<<std::endl;
-//    }
-
- if (controller.GetAButton() == true){
-   if (currentTime - buttonPressedTime > 0.2){
-   buttonPressedTime = currentTime;
-   aunkrModeState = !aunkrModeState;
-   }
- }
-   if (aunkrModeState == true){
-   filtY = filtY/2;
-   filtX = filtX/2;
-   }
-  
+                                                                 //Ankur mode control.
+    //set state 
+    if (controller.GetAButton()){
+        if (currentTime - buttonPressedTime > 0.2){
+            buttonPressedTime = currentTime;
+            aunkrModeState = !aunkrModeState;
+            climbModeState = false;
+        }
+    } else if (controller.GetStartButton()){
+        if(currentTime - buttonPressedTime > 0.2){
+            buttonPressedTime = currentTime;
+            climbModeState = !climbModeState;
+            aunkrModeState = false;
+        }
+    }
     
- 
-  std::cout<<frontRightMotor.GetSelectedSensorPosition()<<std::endl;
-  drivetrain.ArcadeDrive(filtY,filtX);
+    //set drive speed
+    if (aunkrModeState){
+           driveSpeed = leftY * 0.5;
+           turnSpeed = rightX * 0.5;
+           
+
+    } else if (climbModeState){
+        driveSpeed = leftY * 0.3;
+        turnSpeed = rightX * 0.3;
+
+    } else {
+        driveSpeed = filtY;
+        turnSpeed = filtX;
+    }
+    std::cout << driveSpeed << std::endl;
+    //Raw mode control.
+    // if (controller.GetX() == true){
+    //   rawModeState
+    // }
+
+                                                                //Feeder Motor control.
+    frc::SmartDashboard::PutNumber("feed on",feedON);
+    frc::SmartDashboard::PutNumber("feedElapsedTime",feedElpasedTime);
+    // if (controller.GetAButton()){
+    //   feedPeriodic();
+    // }else{
+    //   feedON = false;
+    //   feedElpasedTime = 0;
+    //   prevFeedTime = time.Get();
+    //   feederMotor.Set(ControlMode::PercentOutput,0.0);
+    //  }
+
+    if (controller2.GetTriggerAxis(GenericHID::JoystickHand::kLeftHand) > .1){
+        feederMotor.Set(ControlMode::PercentOutput, controller2.GetTriggerAxis(GenericHID::JoystickHand::kLeftHand));
+        // std::cout<<"FEEDER MOTOR ON"<<std::endl;
+
+    }else if (controller2.GetBumper(GenericHID::JoystickHand::kLeftHand) == true){
+        // std::cout<<"FEEDER MOTOR REVERSE"<<std::endl;
+        feederMotor.Set(ControlMode::PercentOutput,-0.5);
+
+    }else {
+        feederMotor.Set(ControlMode::PercentOutput,0);
+        // std::cout<<"FEEDER MOTOR OFF"<<std::endl;
+
+    }
+                                                                    //Intake Motor control.
+        if(controller.GetYButton()){
+                intakeMotor.Set(ControlMode::PercentOutput,1);
+                std::cout<<"intakeeeeee"<<std::endl;
+        }else	{
+            intakeMotor.Set(ControlMode::PercentOutput,0);
+            std::cout<<"offffffffffffff"<<std::endl;
+    }
+
+
+                                                                    //Shooter motor control.
+    //
+    // double speednew = 0;
+
+    // if (controller.GetBButton()){
+    //     speed = speed + .01;
+    // }else if (controller.GetAButton()){
+    //     speed= speed - .01 ;
+    // }
+
+    // if(controller.GetAButton() == true){
+    //   shooterMotor.Set(ControlMode::PercentOutput,speed);
+    // }else
+    // {
+    //   shooterMotor.Set(ControlMode::PercentOutput,0);
+    // }
+    // frc::SmartDashboard::PutNumber("shootermotor speed", speed);
+
+
+    if (controller2.GetTriggerAxis(GenericHID::JoystickHand::kRightHand) > .1 ){
+        shooterMotor.Set(ControlMode::PercentOutput,-controller2.GetTriggerAxis(GenericHID::JoystickHand::kRightHand));
+        // std::cout<<"SHOOTER MOTOR ON"<<std::endl;
+
+    } else if (controller2.GetBumper(GenericHID::JoystickHand::kRightHand) == true){
+        shooterMotor.Set(ControlMode::PercentOutput, .5);
+        // std::cout<<"SHOOTER MOTOR REVERSE"<<std::endl;
+
+    }else{
+        shooterMotor.Set(ControlMode::PercentOutput, 0);
+        // std::cout<<"SHOOTER MOTOR OFF"<<std::endl;
+
+    }
+
+    frc::SmartDashboard::PutNumber("Shooter Motor", controller2.GetTriggerAxis(GenericHID::JoystickHand::kRightHand));
+
+                                                                        //Elvator Motor control.
+    if (controller.GetBumper(GenericHID::JoystickHand::kLeftHand)){
+        elevatorMotor.Set(ControlMode::PercentOutput, -0.15);
+
+    }else if (controller.GetTriggerAxis(GenericHID::JoystickHand::kLeftHand)) {
+        elevatorMotor.Set(ControlMode::PercentOutput, 0.25);
+
+    }else{
+        elevatorMotor.Set(ControlMode::PercentOutput,0.0);
+    }
+
+                                                                        //Climber Motor control
+    if(controller.GetTriggerAxis(GenericHID::JoystickHand::kRightHand)){
+        climberMotor.Set(ControlMode::PercentOutput,1.0);
+
+    }else if (controller.GetBumper(GenericHID::JoystickHand::kRightHand)){
+        climberMotor.Set(ControlMode::PercentOutput, -0.75);
+
+    }else{
+        climberMotor.Set(ControlMode::PercentOutput, 0.0);
+
+    }
+    
+    
+
+
+                                                                        //limelight code
+    double targetOffsetAngle_Horizontal = table->GetNumber("tx",0.0);
+    double targetOffsetAngle_Vertical = table->GetNumber("ty",0.0);
+    double targetArea = table->GetNumber("ta",0.0);
+    double targetSkew = table->GetNumber("ts",0.0);
+
+    frc::SmartDashboard::PutNumber("ta",targetArea);
+
+
+
+
+    // if (controller2.GetAButton() == true){
+    //     elevatorMotor.Set(ControlMode::PercentOutput, 0.50);
+    //     std::cout<<"25%"<<std::endl;
+
+    // }else if (controller2.GetBButton() == true) {
+    //     elevatorMotor.Set(ControlMode::PercentOutput, 0.5);
+    //     std::cout<<"50%"<<std::endl;
+
+    // }else if (controller2.GetYButton() == true){
+    //     climberMotor.Set(ControlMode::PercentOutput, -0.75);
+    //     std::cout<<"75%"<<std::endl;
+
+    // }else if (controller2.GetXButton() == true){
+    //     climberMotor.Set(ControlMode::PercentOutput, 1.0);
+    //     std::cout<<"100%"<<std::endl;
+
+    // }else{
+    //     climberMotor.Set(ControlMode::PercentOutput,0);
+    //     elevatorMotor.Set(ControlMode::PercentOutput,0);
+    //     std::cout<<"0%"<<std::endl;
+    // }
+    drivetrain.ArcadeDrive(-driveSpeed,turnSpeed);
 
 }
 
-// if (controller.GetX() == true){
-//   rawModeState
-// }
 void Robot::updateEncoderCount(){
-double currentEncoderCount = frontLeftMotor.GetSelectedSensorPosition();
-leftEncoderCount += currentEncoderCount - prevLeftEncoderCount;
-prevLeftEncoderCount = currentEncoderCount;
+    double currentEncoderCount = frontLeftMotor.GetSelectedSensorPosition();
+    leftEncoderCount += currentEncoderCount - prevLeftEncoderCount;
+    prevLeftEncoderCount = currentEncoderCount;
 
-currentEncoderCount = frontRightMotor.GetSelectedSensorPosition();
-rightEncoderCount += currentEncoderCount - prevRightEncoderCount;
-prevRightEncoderCount = currentEncoderCount;
+    currentEncoderCount = frontRightMotor.GetSelectedSensorPosition();
+    rightEncoderCount += currentEncoderCount - prevRightEncoderCount;
+    prevRightEncoderCount = currentEncoderCount;
 
 }
-void Robot::TestPeriodic() {}
+
+
+void Robot::feedPeriodic(){
+    double currentFeedTime = time.Get();
+    feedElpasedTime = feedElpasedTime + (currentFeedTime - prevFeedTime);
+    prevFeedTime = currentFeedTime;
+
+    if(feedON && feedElpasedTime > feedOnTime){
+        feedON = false;
+        feedElpasedTime = 0.0;
+
+    }else if (!feedON && feedElpasedTime > feedOffTime){
+        feedON = true;
+        feedElpasedTime = 0.0;
+
+    }
+    if (feedON){
+        feederMotor.Set(ControlMode::PercentOutput,0.2);
+
+    }else{
+        feederMotor.Set(ControlMode::PercentOutput, 0.0);
+
+    }
+    // std::cout<<"Feed Periodic!!!"<<std::endl;
+}
+
+void Robot::TestPeriodic() {
+
+}
 
 
 #ifndef RUNNING_FRC_TESTS
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -15,6 +15,10 @@
 #include <frc/SpeedControllerGroup.h>
 #include <frc/XboxController.h>
 #include <frc/Timer.h>
+#include "frc/smartdashboard/Smartdashboard.h"
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
+std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
 
 using namespace frc;
 
@@ -29,55 +33,107 @@ class Robot : public frc::TimedRobot {
   void TeleopPeriodic() override;
   void TestPeriodic() override;
   void updateEncoderCount();
+  void feedPeriodic();
+
  private:
-  frc::SendableChooser<std::string> m_chooser;
-  const std::string kAutoNameDefault = "Default";
-  const std::string kAutoNameCustom = "My Auto";
-  std::string m_autoSelected;
+    frc::SendableChooser<std::string> m_chooser;
+    const std::string kAutoNameDefault = "Default";
+    const std::string kAutoNameCustom = "My Auto";
+    std::string m_autoSelected;
 
- bool aunkrModeState = false;
- bool rawModeState = false;
+    XboxController driveController{0};
+    XboxController controlController{1};
+//CONTROLLER VALUES
+bool driveButtonA = driveController.GetAButton();
+bool driveButtonB = driveController.GetBButton();
+bool driveButtonY = driveController.GetYButton();
+bool driveButtonX = driveController.GetXButton();
 
-double encoderCPR = 2048 * 10.71; //one wheel rotation
-double wheelDiameterFt = 6/12; //6 inches
-double rightDistanceTraveledFt = 0;
-double leftDistanceTraveledFt = 0;
-double goalDistance = 10;
+bool controlButtonA = controlController.GetAButton();
+bool controlButtonB = controlController.GetBButton();
+bool controlButtonY = controlController.GetYButton();
+bool controlButtonX = controlController.GetXButton();
+
+// 
+//main robot/
+//DRIVETRAIN
+    WPI_TalonSRX frontLeftMotor{5}; 
+    WPI_TalonSRX backLeftMotor{6};  
+    SpeedControllerGroup leftSideMotors{frontLeftMotor, backLeftMotor};
+
+    WPI_TalonSRX frontRightMotor{1};  
+    WPI_TalonSRX backRightMotor{0}; 
+    SpeedControllerGroup rightSideMotors{frontRightMotor, backRightMotor};
+
+    DifferentialDrive drivetrain{leftSideMotors, rightSideMotors};
+//
+//DRIVETRAIN STATES    
+    bool aunkrModeState = false;
+    bool rawModeState = false;
+    bool climbModeState = false;
+//
+//ACTIONARY MOTORS
+    WPI_TalonSRX shooterMotor{2}; 
+    WPI_TalonSRX feederMotor{3}; 
+    WPI_TalonSRX climberMotor{8};  
+    WPI_TalonSRX elevatorMotor{7};
+    WPI_TalonSRX intakeMotor{9};
+//
+    double encoderCPR = 2048.0 * 10.71; //one wheel rotation
+    double wheelDiameterFt = 0.5; //6 inches
+    double rightDistanceTraveledFt = 0.0;
+    double leftDistanceTraveledFt = 0.0;
+    double goalDistance = 10.0; 
+
+    double driveSpeed = 0;
+    double turnSpeed = 0;
+
+    Timer time;
+    double buttonPressedTime = 0;
+
+    double prevFeedTime = 0.0;
+    double feedOffTime = 1.0;
+    double feedOnTime = 1.0;
+    double feedElpasedTime = 0.0;
+    bool feedON= false;
 
 
 
-  Timer time;
-  double buttonPressedTime = 0;
 
-  XboxController controller{0};
+    //practice.
+    // WPI_TalonSRX shooterMotor{0}; 
+    // WPI_TalonSRX feederMotor{0}; 
+    // WPI_TalonSRX climberMotor{0};  
 
-  WPI_TalonSRX shooterMotor{000000000000000000000};
+    // WPI_TalonSRX frontLeftMotor{4}; 
+    // WPI_TalonSRX backLeftMotor{3};  
+    // SpeedControllerGroup leftSideMotors{frontLeftMotor, backLeftMotor};
 
-  WPI_TalonSRX frontLeftMotor{1};
-  WPI_TalonSRX backLeftMotor{2};
-  SpeedControllerGroup leftSideMotors{frontLeftMotor, backLeftMotor};
+    // WPI_TalonSRX frontRightMotor{1};  
+    // WPI_TalonSRX backRightMotor{2}; 
+    // SpeedControllerGroup rightSideMotors{frontRightMotor, backRightMotor};
 
-  WPI_TalonSRX frontRightMotor{3};
-  WPI_TalonSRX backRightMotor{4};
-  SpeedControllerGroup rightSideMotors{frontRightMotor, backRightMotor};
-
-  DifferentialDrive drivetrain{leftSideMotors, rightSideMotors};
-
-  double leftEncoderCount = 0;
-  double rightEncoderCount = 0;
-  double prevLeftEncoderCount = frontLeftMotor.GetSelectedSensorPosition();
-  double prevRightEncoderCount = frontRightMotor.GetSelectedSensorPosition();
+    // DifferentialDrive drivetrain{leftSideMotors, rightSideMotors};
 
 
-  double prevLeftX = 0;
-  double prevLeftY = 0;
-  // double prevRightX = 0;
-  // double prevRightY = 0;
-  double Tf = 0.25;
+    //Auton Values.
+    double leftEncoderCount = 0;
+    double rightEncoderCount = 0;
+    double prevLeftEncoderCount = frontLeftMotor.GetSelectedSensorPosition();
+    double prevRightEncoderCount = frontRightMotor.GetSelectedSensorPosition();
 
-  double Ts = 1.0/50.0;
-  double a = Ts/(Tf + Ts);
+    double prevRightX = 0;
+    double prevLeftY = 0;
+    // double prevRightX = 0;
+    // double prevRightY = 0;
+    double Tf = 0.30;
+ 
+    double Ts = 1.0/50.0;
+    double a = Ts/(Tf + Ts);
 
-  //DifferentialDrive * difDrive = new DifferentialDrive(motor1, motor2);
+    //DifferentialDrive * difDrive = new DifferentialDrive(motor1, motor2);
+
+
+    double speed = 0.50;
 
 };
